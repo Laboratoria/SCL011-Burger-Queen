97771f38f45900d0df9f2819db8b32bce46e7d0b
María Beltrán <mariabeltra.98@gmail.com>
2019-12-13 16:32:38 -0300

    component navigation

---
+++ src/assets/componets/navigation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
+import './navigation.css'
+
+const Navigation =(props)=>{
+    return(
+        <Router>
+    <nav className="navigation-style">
+    <ul>
+        <li><Link to="/menú">Menú</Link></li>
+        <li><Link to="/listos">Pedidos Listos</Link></li>
+        <li><Link to="/pay">Pagados</Link></li>
+    </ul>
+    </nav>  
+        </Router>
+    
+    )
+};
+export default Navigation;
