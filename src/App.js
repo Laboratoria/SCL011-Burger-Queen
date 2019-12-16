ada5c732387eaf1679f8c9984eeac4f8f9a124b6
CamilaMaira <camilamaira.m@gmail.com>
2019-12-16 12:38:21 -0300

    probando ramas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import data from './data.json';
 
+class App extends Component {
+    constructor () {
+        super ()
+        this.state = {
+            name: "cami"
+        }
+    };
 
+    render () {
+        return <h1>hola {this.state.name}</h1>
+    }
+}
 
 
 export default App;
