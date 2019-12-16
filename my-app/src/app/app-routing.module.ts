e1a60cd9cb6101746534b4ee756ba3ca21c043e5
macarenasaez <makadaira@gmail.com>
2019-12-16 13:56:56 -0300

    btncocina listo

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { startScreenComponent } from './components/startScreen/startScreen.component';
 import {MenuComponent } from './components/menu/menu.component';
+import{PedidosComponent} from './components/pedidos/pedidos.component';
 
 
 
 const ROUTES: Routes = [
 { path: 'home', component:startScreenComponent },
 { path: 'menu', component:MenuComponent },
+{ path: 'pedidos', component:PedidosComponent},
 { path: '', pathMatch: 'full', redirectTo: 'home' },
 { path: '**', component: startScreenComponent },
 ];
