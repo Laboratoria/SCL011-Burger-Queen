9d1865db864d8236f57c965f6efbb2ff919e4909
CamilaMaira <camilamaira.m@gmail.com>
2019-12-17 13:54:44 -0300

    inicio de sesion con firebase ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import './App.css';
+import { useFirebaseApp, useUser } from "reactfire";
+import Auth from "./Auth";
 
+function App () {
+    const firebase = useFirebaseApp();
+    console.log(firebase)
 
+    const user = useUser();
 
+    return (
+        
+        <div className="user-app">
+            { user && <p>Usuario: {user.email}</p> }
+            <Auth />
+        </div>
+
+    )
+}
 
 export default App;
+
