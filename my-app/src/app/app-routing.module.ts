256e64258e70aa907f81605e9c2e3354b4b747ae
macarenasaez <makadaira@gmail.com>
2019-12-18 11:07:18 -0300

    archivos recuperados

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { startScreenComponent } from './components/startScreen/startScreen.component';
 import {MenuComponent } from './components/menu/menu.component';
-import{PedidosComponent} from './components/pedidos/pedidos.component';
+import { PedidosComponent } from "../app/components/pedidos/pedidos.component";
 
 
 
 const ROUTES: Routes = [
 { path: 'home', component:startScreenComponent },
+{ path: 'pedidos', component: PedidosComponent},
 { path: 'menu', component:MenuComponent },
-{ path: 'pedidos', component:PedidosComponent},
-{ path: '', pathMatch: 'full', redirectTo: 'home' },
-{ path: '**', component: startScreenComponent },
-];
+{ path: '**', pathMatch: 'full', redirectTo: 'home' },
 
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
