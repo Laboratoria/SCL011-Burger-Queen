9a2a7ceb184a9ea2642ce4039f27f2aea7189d10
martcamila1 <53873117+martcamila1@users.noreply.github.com>
2019-12-18 12:16:10 -0300

    primer resumen menu almuerzo (#6)

--- src/assets/components/buttonmenu.js
+++ src/assets/components/buttonmenu.js
@@ -6,7 +6,8 @@ class ButtonMenu extends Component{
     
     render(){
       return  <div>
-    <button className="buttonMenu" onClick={this.props.clickBtn}>{this.props.productProp.product}-{this.props.productProp.price}</button>
+        <button className="buttonMenu" onClick={this.props.clickBtn.bind(this, this.props.productProp)}>{this.props.productProp.product}-{this.props.productProp.price}</button>
+    
       </div>
     }
   }
--- src/assets/components/menulunch.js
+++ src/assets/components/menulunch.js
@@ -7,15 +7,48 @@ class MenuLunch extends Component{
     super(props);
      this.state={
    
-    json:json1.Lunch
+    json:json1.Lunch,
+    price:[],
+    product:[]
   }
 }
+clickBtn2=(producto)=>{
+   
+    let productss = this.state.product;
+    productss.push(producto);
+     
+     this.setState({
+       product: productss
+     }); 
+    
+     let totalPrice =this.state.product.map(e=>e.price)
+     
+     const total = totalPrice.reduce((acum, elemt) => acum + elemt, 0)
+     console.log(total)
+     
+     let precios = this.state.price;
+     precios.push(total)
+    
+     this.setState({
+      price: precios
+    }); 
+     console.log(this.state.price);
+     
+     console.log("ESTADO:", this.state.product);
+    }
   render(){
     
-    return <div>
-    
-      {this.state.json.map((element, i) => <ButtonMenu key={element.id}  productProp={element}  />)}
-  
+    return <div> 
+    {this.state.json.map((element) => ( 
+    <ButtonMenu key={element.id} clickBtn={this.clickBtn2} productProp={element} />
+    ))}
+   {this.state.product.map(e=> {return <div>
+   <p key ={e.id}>{e.price}{e.product}</p>
+   <button >Eliminar</button>
+   </div>
+   })}
+   
+  <p>{this.state.price[this.state.price.length - 1]}</p>
     </div>
   }
 }
