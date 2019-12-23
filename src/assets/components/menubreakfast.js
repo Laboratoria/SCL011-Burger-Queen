e06d38c55bb79f6cbe40a8e0113d19e6771af728
martcamila1 <53873117+martcamila1@users.noreply.github.com>
2019-12-23 17:24:14 -0300

    Fx borrar y suma (#11)

--- src/assets/components/menubreakfast.js
+++ src/assets/components/menubreakfast.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import json1 from '../../data.json'
 import ButtonMenu from './buttonmenu'
+import InputName from './input.js'
+import Combobox from './combobox.js'
 
 class MenuBreakFast extends Component{
   constructor(props){
@@ -12,16 +14,16 @@ class MenuBreakFast extends Component{
     product: []
   }
 }
-clickBtn2=(producto)=>{
+clickBtn2=(productMenu)=>{
    
-  let productss = this.state.product;
-  productss.push(producto);
+  let productState = this.state.product;
+  productState.push(productMenu);
    
    this.setState({
-     product: productss
+     product: productState
    }); 
   
-   let totalPrice =this.state.product.map(e=>e.price)
+   let totalPrice =this.state.product.map(elementProduct=>elementProduct.price)
    
    const total = totalPrice.reduce((acum, elemt) => acum + elemt, 0)
    console.log(total)
@@ -36,19 +38,43 @@ clickBtn2=(producto)=>{
    
    console.log("ESTADO:", this.state.product);
   }
+
+  //funcion para borrar post
+  remove(index,price ) {
+  
+    const removeProduct = this.state.product.filter((element ,i)=>{
+    return i !==index 
+    
+  })
+  
+    this.setState({
+    product:removeProduct,
+    
+  })
+
+    let reducePrice= (this.state.price[this.state.price.length - 1] -price);
+    let statePrice =this.state.price
+    statePrice.push(reducePrice) 
+
+    this.setState({
+    price:statePrice
+  })
+  
+}
   render(){
     
     return <div>
-     
+      <Combobox/>
+      <InputName/>
       {this.state.json1.map((element) => (
       <ButtonMenu key={element.id} clickBtn={this.clickBtn2} productProp={element} />
       ))}
-      {this.state.product.map(elementProduct=> { return <div>
-      <p key ={elementProduct.id}>{elementProduct.price}{elementProduct.product}</p>
-      <button >Eliminar</button>
-      </div>
-
-      })}
+       {this.state.product.map((elementProduct,i)=> {return <div>
+   <p key ={elementProduct.id}>{elementProduct.price}{elementProduct.product}</p>
+   <button onClick={this.remove.bind(this, i ,elementProduct.price)} >Eliminar</button>
+   </div>
+   })}
+   <p>{this.state.price[this.state.price.length - 1]}</p>
     </div>
   }
 }
