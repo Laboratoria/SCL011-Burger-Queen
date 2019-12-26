9332820ea1b2af3369f3b8f9ab6e59458005a9d9
crusiris <cruzsirys@gmail.com>
2019-12-26 11:47:33 -0300

    Funcionalidad y estilos  del boton Mesa

--- my-app/src/app/components/mesas/mesas.component.ts
+++ my-app/src/app/components/mesas/mesas.component.ts
@@ -14,7 +14,7 @@ export class MesasComponent implements OnInit {
   tabls: TablsModel[] = [];
   
 
-  //Inyecto mi servicio
+  //Inyecto mi servicio dentro del contructor para que la data cargue antes de la interfaz
   constructor(private waiqueenService:WaiqueenService) { }
 
   ngOnInit() {
@@ -27,5 +27,10 @@ export class MesasComponent implements OnInit {
           console.log(this.tabls);
             });
           
+  }
+
+  //Funcion obtener numero de mesa
+  getTable(tableNumber:number){
+    console.log(tableNumber);
   }
 }
