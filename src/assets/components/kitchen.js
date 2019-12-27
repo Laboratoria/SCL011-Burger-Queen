e7b841bcb25fb81aecdb07fed16cb7eea3b13142
martcamila1 <martcamila1@gmail.com>
2019-12-27 13:43:56 -0400

    agrego carpeta kitchen

---
+++ src/assets/components/kitchen.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Kitchen extends Component{
+    render(){
+       return <div>Hola soy cocina</div>
+
+    }
+}
+export default Kitchen
---
+++ src/assets/views/waiter.js
@@ -0,0 +1,1 @@
+// home para mesero
