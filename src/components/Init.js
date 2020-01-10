324b5a95736da3588b9410993adde90d4afae93f
danielaolivares <dany.olivaresl@gmail.com>
2020-01-10 08:32:26 -0400

    agrego css primera pantalla

--- src/components/Init.js
+++ src/components/Init.js
@@ -1,11 +1,19 @@
 import React from "react";
 
+import Logo from "../images/logopequeño.png";
+import "./styles/Init.css";
+
 class Init extends React.Component {
   render() {
     return (
-      <div>
-        <button>Mesero</button>
-        <button>Cocinero</button>
+      <div className="all">
+        <div>
+          <img src={Logo} alt="Logo" />
+        </div>
+        <div>
+          <button>Mesero</button>
+          <button>Cocinero</button>
+        </div>
       </div>
     );
   }
